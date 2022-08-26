0fa06867c45c1ac2cb42192a2e9e0739ae7bd8bf
gogpg <giedre.luko@gmail.com>
2022-08-26 18:51:03 +0300

    button clicked

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -2,11 +2,21 @@
 import './App.scss';
 
 function App() {
+
+    const clicked = () => {
+        console.log('labas')
+        doJob()
+    }
+    const doJob = () => {
+        console.log('hello')
+    }
+
   return (
     <div className="App">
         
       <header className="App-header">
       <h1>STATE</h1>
+      <button onClick = {clicked}>Click</button>
       </header>
     </div>
   );

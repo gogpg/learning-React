51dc6a527df9b2e5f8863e290b165956e23b2e4e
gogpg <giedre.luko@gmail.com>
2022-09-14 14:11:26 +0300

    class work

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.scss';
 import Color3 from './Components/017/Color3';
+import Number from './Components/017/Number';
+import Ten from './Components/017/Ten';
 
 
 
@@ -10,6 +12,8 @@ function App() {
             <header className="App-header">
             <h1>UseReducer</h1>
             <Color3/>
+            <Number/>
+            <Ten/>
             </header>
         </div>
     );

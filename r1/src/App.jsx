b7dcdc40f134144f1e0508e4d17cc81c6b464118
gogpg <giedre.luko@gmail.com>
2022-09-15 12:16:20 +0300

    API data sort

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import Api1 from './Components/018/Api1';
+import Api2 from './Components/018/Api2';
 
 
 
@@ -10,6 +11,7 @@ function App() {
             <header className="App-header">
             <h1>API CALL</h1>
             <Api1/>
+            <Api2/>
             </header>
         </div>
     );
---
+++ r1/src/Components/018/Api2.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import axios from 'axios';
+import { useEffect } from "react";
+
+function Api2() {
+
+    const [posts, setPosts] = useState([])
+
+    // useEffect(() => {
+    //     axios.get('https://jsonplaceholder.typicode.com/users')
+    //     .then(res => setUsers(res.data);
+    //     })
+    // }, []);
+
+    useEffect(() => {                                           ///useEffect uzsikrauna, kai uzsikrauna komponentas.
+        axios.get('https://jsonplaceholder.typicode.com/posts')  //kreipiames i axios, kad gautume duomenis is norimo adreso/serverio, kuri ir irasome skliausteliuose
+        .then(res => {                                           //kai gauna atsakyma, tada setina i users, atsakymas yra su daug nereikalingos papildomos info, todel is to responso paimame tik data.
+            console.log(res);
+            setPosts(res.data.sort((a, b) => b.title.length - a.title.length));    
+        })
+    }, []);
+
+    return (
+        <ul>
+            {
+                posts.map(u => <li key = {u.id}>Users title: {u.title}</li>)    
+            }
+        </ul>
+    )
+}
+export default Api2;

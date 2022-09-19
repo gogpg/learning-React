691a2e9d4207fc55cabc3540dec75a45e1cfd2c6
gogpg <giedre.luko@gmail.com>
2022-09-19 22:05:59 +0300

    setTimeout

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect } from 'react';
 import { useState } from 'react';
 import rand from './Functions/rand';
@@ -11,6 +10,8 @@ function App() {
     const [counter3, setCounter3] = useState(0);
     const [counter4, setCounter4] = useState([rand(10, 99), rand(10, 99)]);
     const [counter5, setCounter5] = useState(0);
+    const [color, setColor] = useState('skyblue');
+    const [counter6, setCounter6] = useState('');
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -52,19 +53,37 @@ function App() {
             clearInterval(intervalId);
         }
     }, []);
-    
-           
+
+    const changeColor = () => {
+        const intervalId = setInterval(() => {
+            setCounter6(c => {
+                const n = c === '' ? 9 : c - 1;
+                if (n === 0) {
+                    clearInterval(intervalId);
+                }
+                return n;
+            });
+        }, 100);
+        setTimeout(() => {
+            setColor('crimson');
+        }, 1000);
+    }
+
+
     return (
         <div className="App">
             <header className="App-header">
-            <h2>{counter}</h2>
-            <h2>{counter2}</h2>
-            <h2>{counter4[0]}-{counter4[1]} {counter5}</h2>
-            <div className="containerBin">
-                {
-                    [...Array(counter3)].map((_, i) => <div key={i}></div>)
-                }
-            </div>
+                <h2>{counter}</h2>
+                <h2>{counter2}</h2>
+                <h2>{counter4[0]}-{counter4[1]} {counter5}</h2>
+                <div className="container">
+                    <div className = 'kv' style={{ backgroundColor: color }} onClick={changeColor}>{counter6}</div>
+                </div>
+                <div className="container">
+                    {
+                        [...Array(counter3)].map((_, i) => <div className = 'kv' key={i}></div>)
+                    }
+                </div>
             </header>
         </div>
     );
